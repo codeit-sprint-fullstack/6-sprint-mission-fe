a208b5e24e169949620befee9eb871ccadaf38c0
Muffin <chanbin79@naver.com>
2025-02-12 20:26:28 +0900

    signup.js(이메일,비밀번호 형식오류)

---
+++ panda market/js/login.js
@@ -0,0 +1,60 @@
+function emailchange() {
+    const emInput = document.querySelector(".emailinput");
+    console.log(emInput)
+
+    const emInputData = emInput.value
+    console.log(emInputData)
+
+    const emPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    console.log(emPattern)
+
+    const emError = document.querySelector(".emailerror")
+    console.log(emError)
+
+    // const emInputError = document.querySelector(".inputerror")
+
+    // if (emInputError.test("")) {
+    //     emError.style.visibility = "visible";
+    //     emInput.style.borderColor = 'red';
+    // } else {
+    //     emError.style.visibility = "hidden";
+    //     emInput.style.borderColor = "transparent";
+    //     return true;
+    // }
+     if (emPattern.test(emInputData)) {
+        emError.style.visibility = "hidden";
+        emInput.style.borderColor = "transparent"
+    } else {
+        emError.style.visibility = "visible";
+        emInput.style.borderColor = 'red';
+    }
+} 
+
+
+function passwordchange() {
+    console.log(document.getElementsByClassName("passwordinput")[0].value)
+    const pwInput = document.querySelector(".passwordinput"); // 첫 번째 input 선택
+    console.log(pwInput.value.length); // 입력된 값의 길이 출력
+
+    const pwlength = pwInput.value.length;
+
+    const passwordError = document.querySelector(".passworderror");
+    console.log(passwordError)
+    
+    if (pwlength < 8) {
+        passwordError.style.visibility = "visible";
+        pwInput.style.borderColor = 'red';
+    } else {
+        passwordError.style.visibility = "hidden";
+        pwInput.style.borderColor = 'transparent';
+    }
+}
+
+const USER_DATA = [
+    { email: 'codeit1@codeit.com', password: "codeit101!" },
+    { email: 'codeit2@codeit.com', password: "codeit202!" },
+    { email: 'codeit3@codeit.com', password: "codeit303!" },
+    { email: 'codeit6@codeit.com', password: "codeit606!" },
+    { email: 'codeit4@codeit.com', password: "codeit404!" },
+    { email: 'codeit5@codeit.com', password: "codeit505!" },
+]
--- panda market/js/signup.js
+++ panda market/js/signup.js
@@ -1,12 +1,16 @@
-function emailchange() {
+function signupEmChanger() {
     const emInput = document.querySelector(".emailinput");
     console.log(emInput)
+
     const emInputData = emInput.value
     console.log(emInputData)
+
     const emPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     console.log(emPattern)
+
     const emError = document.querySelector(".emailerror")
     console.log(emError)
+
     if (emPattern.test(emInputData)) {
         emError.style.visibility = "hidden";
         emInput.style.borderColor = "transparent"
@@ -14,17 +18,20 @@ function emailchange() {
         emError.style.visibility = "visible";
         emInput.style.borderColor = 'red';
     }
-} 
 
+}
 
 function passwordchange() {
     console.log(document.getElementsByClassName("passwordinput")[0].value)
     const pwInput = document.querySelector(".passwordinput"); // 첫 번째 input 선택
     console.log(pwInput.value.length); // 입력된 값의 길이 출력
+
     const pwlength = pwInput.value.length;
+
     const passwordError = document.querySelector(".passworderror");
     console.log(passwordError)
-    if (pwlength <= 8) {
+    
+    if (pwlength < 8) {
         passwordError.style.visibility = "visible";
         pwInput.style.borderColor = 'red';
     } else {
@@ -32,12 +39,3 @@ function passwordchange() {
         pwInput.style.borderColor = 'transparent';
     }
 }
-
-const USER_DATA = [
-    { email: 'codeit1@codeit.com', password: "codeit101!" },
-    { email: 'codeit2@codeit.com', password: "codeit202!" },
-    { email: 'codeit3@codeit.com', password: "codeit303!" },
-    { email: 'codeit6@codeit.com', password: "codeit606!" },
-    { email: 'codeit4@codeit.com', password: "codeit404!" },
-    { email: 'codeit5@codeit.com', password: "codeit505!" },
-]
