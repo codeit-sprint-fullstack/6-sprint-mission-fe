e29224ce6f66c086eb3d537336c294e5414d320b
Jam1eL1 <23jisu@gmail.com>
2025-02-12 10:49:49 +0900

    feat: add user data provided from assignment requirement

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,3 +1,13 @@
+//  user database
+const USER_DATA = [
+  { email: 'codeit1@codeit.com', password: "codeit101!" },
+  { email: 'codeit2@codeit.com', password: "codeit202!" },
+  { email: 'codeit3@codeit.com', password: "codeit303!" },
+  { email: 'codeit4@codeit.com', password: "codeit404!" },
+  { email: 'codeit5@codeit.com', password: "codeit505!" },
+  { email: 'codeit6@codeit.com', password: "codeit606!" },
+];
+
 // handling aut__form-password-toggle for password input visibility 
 document.querySelectorAll(".auth__form-password-toggle").forEach((btn) => {
 btn.addEventListener("click", ()=>{
@@ -7,3 +17,7 @@ btn.addEventListener("click", ()=>{
   }
 });
 });
+
+let form = document.querySelector('.auth__form');
+let emailField = document.querySelector('#email');
+let passwordField = document.querySelector('#password');
