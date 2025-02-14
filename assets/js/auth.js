8bda445da68caba3a2ef4ae1c4edd9a2390a193a
Jam1eL1 <23jisu@gmail.com>
2025-02-14 20:40:55 +0900

    refactor: improve password visibility toggle function to update toggleIcon source, alt, userInput type value properly

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,9 +1,18 @@
-// handling aut__form-password-toggle for password input visibility 
-document.querySelectorAll(".auth__form-password-toggle").forEach((btn) => {
-btn.addEventListener("click", ()=>{
-  const userInput = btn.previousElementSibling;
-  if (userInput && userInput.classList.contains("auth__form-password-input")) {
-    userInput.type = userInput.type === "password"? "text" : "password";
-  }
-});
-});
+const passwordToggleBtn = document.querySelector(".auth__form-password-toggle");
+if (passwordToggleBtn) {
+  passwordToggleBtn.addEventListener("click", () => {
+    const userInput = passwordToggleBtn.previousElementSibling;
+    const toggleIcon = passwordToggleBtn.querySelector("img");
+    if (userInput && userInput.classList.contains("auth__form-password-input")) {
+      if (userInput.type === "password") {
+        userInput.type = "text";
+        toggleIcon.src = "assets/img/icons/eye-visible.svg";
+        toggleIcon.alt = "비밀번호 숨기기 아이콘";
+      } else {
+        userInput.type = "password";
+        toggleIcon.src = "assets/img/icons/eye-invisible.svg";
+        toggleIcon.alt = "비밀번호 표시 아이콘";
+      }
+    }
+  });
+}
