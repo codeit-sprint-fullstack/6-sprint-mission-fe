feda11e76e93b02739eeee699692db570e31d39a
rakaso598 <nexonaddress98@gmail.com>
2025-02-19 09:47:38 +0900

    feat: add getArticleList

--- frontend/js/article.js
+++ frontend/js/article.js
@@ -1,23 +1,11 @@
 import axios from "axios";
 
-const url = new URL("https://sprint-mission-api.vercel.app/articles"); // API
+// https://panda-market-api-crud.vercel.app/docs 명세표
 
-const getArticleList = () => {
-  axios.get(url);
+const getArticleListURL = 'https://panda-market-api-crud.vercel.app/products?page=1&pageSize=10&orderBy=recent';
+const getArticleList = async () => {
+  const res = await axios.get(getArticleListURL);
+  return res.data;
 };
-console.log(getArticleList());
-
-const getArticle = () => {
-  axios.get(url);
-};
-console.log(getArticle());
-
-const patchArticle = () => {
-  axios.patch(url);
-};
-console.log(patchArticle());
-
-const deleteArticle = () => {
-  axios.delete(url);
-};
-console.log(deleteArticle());
+const list = await getArticleList();
+console.log(list);
