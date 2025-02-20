6e7c62b021109cad2c2271b16d47304c8a49d687
Dani Kim <127651317+danikim8@users.noreply.github.com>
2025-02-20 17:50:40 +0900

    Update ArticleService.js

--- services/ArticleService.js
+++ services/ArticleService.js
@@ -1,4 +1,4 @@
-//fetch, .then()메서드, .catch() 사용
+//fetch, .then()메서드/비동기처리, .catch()/오류처리 사용
 const BASE_URL = new URL('https://sprint-mission-api.vercel.app/articles');
 
 function getArticleList(page = 1, pageSize = 100, keyword = "") {
