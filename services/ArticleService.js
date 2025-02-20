1c0396540bbddfcd77d025429105664938b24fbb
danikim8 <202115057@sangmyung.kr>
2025-02-20 19:48:26 +0900

    Add Product API

--- services/ArticleService.js
+++ services/ArticleService.js
@@ -1,5 +1,5 @@
 //fetch, .then()메서드/비동기처리, .catch()/오류처리 사용
-const BASE_URL = new URL('https://sprint-mission-api.vercel.app/articles');
+const BASE_URL = 'https://sprint-mission-api.vercel.app/articles';
 
 function getArticleList(page = 1, pageSize = 100, keyword = "") {
 
@@ -12,7 +12,7 @@ function getArticleList(page = 1, pageSize = 100, keyword = "") {
 
   return fetch(url)
     .then(response => {
-      if(!response.ok){//!200, 400/500 에러발생생
+      if(!response.ok){//!200, 400/500 에러발생
         throw new Error(`[ERROR] getArticleList failed: ${response.status}`);
       }
       return response.json();
