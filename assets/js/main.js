9a64149672e4084c51aaf09bdd43a283041f463c
Jam1eL1 <23jisu@gmail.com>
2025-02-21 11:24:24 +0900

    refactor: replace double quote with single quote in main.js for consistency - use single quotes going forward

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,29 @@
 //  need to import functions from ArticleService.js and ProductService.js
-import { createArticle, getArticleList, getArticle } from "./ArticleService.js";
+import { createArticle, getArticleList, getArticle } from './ArticleService.js';
 
 // test articleContent vars for createArticle
 const articleContent = {
-  "title": "F1 Ferrari Driver - Lewis Hamilton",
-  "content": "This is part of mission 4",
-  "image": "https://media.formula1.com/image/upload/f_auto,c_limit,q_auto,w_1320/content/dam/fom-website/drivers/2025Drivers/hamilton"
+  'title': 'F1 Ferrari Driver - Lewis Hamilton',
+  'content': 'This is part of mission 4',
+  'image': 'https://media.formula1.com/image/upload/f_auto,c_limit,q_auto,w_1320/content/dam/fom-website/drivers/2025Drivers/hamilton'
 };
 
 const articleContent2 = {
-  "title": "F1 Red Bull Racing Driver - Max Verstappen",
-  "content": "Box Box",
-  "image": "https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/drivers/2024Drivers/verstappen"
+  'title': 'F1 Red Bull Racing Driver - Max Verstappen',
+  'content': 'Box Box',
+  'image': 'https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/drivers/2024Drivers/verstappen'
 };
 
 const articleContent3 = {
-  "title": "F1 Williams Driver - Carlos Sainz",
-  "content": "Box Box", // try to patch this content info and below image link 
-  "image": "https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/drivers/2024Drivers/verstappen" 
+  'title': 'F1 Williams Driver - Carlos Sainz',
+  'content': 'Box Box', // try to patch this content info and below image link 
+  'image': 'https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/drivers/2024Drivers/verstappen' 
 };
 
 const articleContent4 = {
-  "title": "F1 McLaren Driver - Lando Norris", // Different title
-  "content": "It's lights out and away we go!", // Different content
-  "image": "https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/drivers/2024Drivers/norris" // Different image
+  'title': 'F1 McLaren Driver - Lando Norris', // Different title
+  'content': 'It\'s lights out and away we go!', // Different content
+  'image': 'https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/drivers/2024Drivers/norris' // Different image
 };
 
 
@@ -32,7 +32,7 @@ const articleContent4 = {
 async function init() {
   // test getArticleList 
   try {
-  const list = await getArticleList("Lewis Hamilton");
+  const list = await getArticleList('Lewis Hamilton');
 
   // test getArticle 
   const id = 555;
