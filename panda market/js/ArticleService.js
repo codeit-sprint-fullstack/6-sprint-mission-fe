7ec648c8fac250500ca0b9858e8207abc91c95cd
Muffin <chanbin79@naver.com>
2025-02-23 10:39:27 +0900

    ArticleService.js 완성

--- panda market/js/ArticleService.js
+++ panda market/js/ArticleService.js
@@ -1,27 +1,26 @@
 import axios from 'axios'
 
-const BASE_URL = 'https://panda-market-api-crud.vercel.app';
+export const BASE_URL = 'https://panda-market-api-crud.vercel.app/articles';
 
-// const getArticleList = (page, pageSize, keyword) => {
-//   console.log("함수실행중")
-//   const res = BASE_URL + "/articles"
-//     return axios.get(res, {
-//     params: { page: page, pageSize: pageSize, keyword: keyword }
-//   })
-//   .then(response => {
-//     console.log(response.data);
-//     return response.data;
-//   })
-//   .catch(error => {
-//     console.log(error.response.status)
-//     console.log(error.response.data)
-//   })
-// }
-// getArticleList();
-
-const getArticle = (id) => {
+export const getArticleList = (page, pageSize, keyword) => {
   console.log("함수실행중")
-  const res = BASE_URL + `/articles/${id}`;
+  const res = BASE_URL;
+    return axios.get(res, {
+    params: { page: page, pageSize: pageSize, keyword: keyword }
+  })
+  .then(response => {
+    console.log(response.data);
+    return response.data;
+  })
+  .catch(error => {
+    console.log(error.response.status)
+    console.log(error.response.data)
+  })
+}
+
+export const getArticle = (id) => {
+  console.log("함수실행중")
+  const res = `BASE_URL/${id}`;
     return axios.get(res)
   .then(response => {
     console.log(response.data);
@@ -32,4 +31,53 @@ const getArticle = (id) => {
     // console.log(error.response.data)
   })
 }
-getArticle();
+
+export const createArticle = (title, content, image) => {
+  console.log("함수실행중중")
+  const res = BASE_URL;
+  const articleData = {
+    title: title,
+    content: content,
+    image: `https://${image}`
+    };
+    return axios.post(res, articleData)
+      .then(response => {
+        console.log("응답 데이터", response.data)
+        return response.data;
+      })
+      .catch(error => {
+        console.error("오류 발생:", error.response ? error.response.data : error.message);
+        return null;
+      });
+}
+
+export const patchArticle = (id, title, content, image) => {
+  const res = `${BASE_URL}/${id}`;
+  const articleData = {
+    title: title,
+    content: content,
+    image: `https://${image}`
+  };
+  return axios.patch(res, articleData)
+    .then(response => {
+      console.log("응답 데이터", response.data)
+      return response.data;
+    })
+    .catch(error => {
+      console.error("오류 발생:", error.response ? error.response.data : error.message);
+    })
+
+}
+
+export const deleteArticle = (id) => {
+  const res = `${BASE_URL}/${id}`;
+  return axios.delete(res)
+   .then(response => {
+    console.log("응답 데이터", response.data)
+    return response.data;
+   })
+   .catch(error => {
+    console.error("오류 발생:", error.response ? error.response.data : error.message);
+   })
+}
+
