d039c3cbe20024fbf12af855e2c96bb72c6adcc4
fiivxyxxng <scx200800@gmail.com>
2025-03-05 11:25:14 +0900

    feat: 하위 컴포넌트들 App 컴포넌트에 추가 및 handleLoad 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import Header from "./components/Header";
+import BestProduct from "./components/BestProduct";
+import Product from "./components/Product";
+import ProductList from "./components/ProductList";
+import Footer from "./components/Footer";
+import { useEffect, useState } from "react";
+import { getProducts } from "./api/api";
+
 function App() {
-  return <div></div>;
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    handleLoad();
+  }, []);
+
+  const handleLoad = async () => {
+    const { list } = await getProducts();
+    setItems(list);
+  };
+
+  return (
+    <div>
+      <Header />
+      <BestProduct items={items} />
+      <Product />
+      <ProductList items={items} />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
