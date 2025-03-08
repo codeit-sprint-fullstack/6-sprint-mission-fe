beae065f6ffc5a2eaa3ecaa76c3f43a700a868ae
fiivxyxxng <scx200800@gmail.com>
2025-03-09 01:40:52 +0900

    refactor: api 수정

--- src/api/api.js
+++ src/api/api.js
@@ -1,19 +1,17 @@
 const baseUrl = "https://panda-market-api.vercel.app/products";
 
 export const getProducts = async (
-  page = 1,
-  pageSize = 10,
-  orderBy = "recent",
-  keyword
+  page,
+  pageSize,
+  keyword,
+  orderBy = "recent"
 ) => {
   try {
     const url = new URL(baseUrl);
-    url.searchParams.append("page", page);
-    url.searchParams.append("pageSize", pageSize);
+    if (page) url.searchParams.append("page", page);
+    if (pageSize) url.searchParams.append("pageSize", pageSize);
+    if (keyword) url.searchParams.append("keyword", keyword);
     url.searchParams.append("orderBy", orderBy);
-    if (keyword) {
-      url.searchParams.append("keyword", keyword);
-    }
     const res = await fetch(url);
     if (!res.ok) {
       console.error(res.message);
