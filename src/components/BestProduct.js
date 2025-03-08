170a140e2dd4e288e08865b948b55d26b4b31bb2
fiivxyxxng <scx200800@gmail.com>
2025-03-09 01:45:39 +0900

    feat: bestProduct 컴포넌트 구현 및 css 추가

--- src/components/BestProduct.js
+++ src/components/BestProduct.js
@@ -1,32 +1,51 @@
-import "./ProductList.css";
+import "./BestProduct.css";
+import unheartIcon from "../assets/images/icon/ic-unheart.svg";
+import { useEffect, useState } from "react";
 
 function BestProduct({ items }) {
+  const [pageSize, setPageSize] = useState(4);
+
+  useEffect(() => {
+    const updateBestItemCount = () => {
+      if (window.matchMedia("(max-width: 743px)").matches) {
+        setPageSize(1);
+      } else if (window.matchMedia("(max-width: 1199px)").matches) {
+        setPageSize(2);
+      } else {
+        setPageSize(4);
+      }
+    };
+
+    updateBestItemCount();
+
+    window.addEventListener("resize", updateBestItemCount);
+    return () => {
+      window.removeEventListener("resize", updateBestItemCount);
+    };
+  }, []);
+
   const bestItems = items
     .sort((a, b) => b.favoriteCount - a.favoriteCount)
-    .slice(0, 4);
+    .slice(0, pageSize);
 
   return (
-    <>
-      <nav>
-        <div>베스트 상품</div>
-      </nav>
-      <div className="best-product-list">
+    <main className="bestProduct">
+      <div className="title">베스트 상품</div>
+      <div className="list">
         {bestItems.map((item) => (
           <div key={item.id}>
-            <img
-              className="best-product-list__img"
-              src={item.images}
-              alt={item.name}
-            />
-            <div>
-              <div>{item.name}</div>
-              <div>{item.price}원</div>
-              <div>좋아요 {item.favoriteCount}</div>
+            <img className="img" src={item.images} alt={item.name} />
+            <div className="description">
+              <div className="name">{item.name}</div>
+              <div className="price">{item.price}원</div>
+              <div className="favoriteCount">
+                <img src={unheartIcon} /> {item.favoriteCount}
+              </div>
             </div>
           </div>
         ))}
       </div>
-    </>
+    </main>
   );
 }
 
