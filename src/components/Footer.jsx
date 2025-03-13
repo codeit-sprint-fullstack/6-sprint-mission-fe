5bf342449b43a02a7dd0053cda66b56dbe0c5ce8
jbinyim <jbinyim12@naver.com>
2025-03-13 20:47:35 +0900

    250313

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -25,7 +25,7 @@ const Footer = () => {
           />
           <img
             src="https://67a1772da019bb307bce76e3--relaxed-puppy-88a9cf.netlify.app/imges/ic_instagram.png"
-            alt="insta"
+            alt="instagrame"
           />
         </div>
       </div>
