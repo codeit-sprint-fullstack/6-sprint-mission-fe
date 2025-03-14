272e7a69f8914e9153f51b6ab3b59b986caa4bc3
jyeon <bjlyeon2003@naver.com>
2025-03-14 13:09:40 +0900

    중고마켓페이지 '/' 설정

--- Sprint-5/src/App.jsx
+++ Sprint-5/src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Header } from "./component/common/Header";
 import { Footer } from "./component/common/Footer";
 import BestItem from "./component/items/BestItem";
@@ -5,12 +6,18 @@ import { SellItem } from "./component/items/SellItem";
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-      <BestItem />
-      <SellItem />
-      <Footer />
-    </>
+      <Routes>
+        <Route path="/" element={ 
+          <>
+            <BestItem /> 
+            <SellItem />
+          </>
+        } />
+      </Routes>
+      <Footer /> 
+    </Router>
   );
 }
 
