e0d86ccd55e97c4b9dd29933f22f2f5f509a3cba
root <root@Gxng.>
2025-03-14 11:57:53 +0900

    M6-4.1

--- src/pages/RegistrationPage.jsx
+++ src/pages/RegistrationPage.jsx
@@ -6,14 +6,63 @@ import {
   PatchProduct,
   DeleteProduct,
 } from "../api/ProductService";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const RegistrationPage = () => {
   const [itemTitle, setItemTitle] = useState("");
+  const [itemContent, setItemContent] = useState("");
+  const [isContentError, setIsError] = useState(false);
+  const [itemPrice, setItemPrice] = useState("");
+  const [itemTag, setItemTag] = useState("");
 
-  const handleiTitleChange = (e) => {
-    setItemTitle(e.target.value);
-    console.log(itemTitle);
+  //item Title의 input border 핸들러
+  const handleChangeItemTitle = (e) => {
+    const value = e.target.value;
+    setItemTitle(value);
+    if (value.trim().length > 10) {
+      e.target.style.border = "1px solid #f74747";
+    } else {
+      e.target.style.border = "";
+    }
+  };
+
+  //item content의 input border 핸들러
+  const handleChangeItemContent = (e) => {
+    const value = e.target.value;
+    setItemContent(value);
+    if (value.trim().length < 10) {
+      e.target.style.border = "1px solid #f74747";
+    } else {
+      e.target.style.border = "";
+    }
+  };
+  //item content의 error 메세지 T/F
+  useEffect(() => {
+    itemContent.trim().length < 10 && itemContent.trim().length > 0
+      ? setIsError(true)
+      : setIsError(false);
+  }, [itemContent]);
+
+  //item Price의 input border 핸들러
+  const handleChangeItemPrice = (e) => {
+    const value = e.target.value;
+    setItemPrice(value);
+    if (isNaN(itemPrice)) {
+      e.target.style.border = "1px solid #f74747";
+    } else {
+      e.target.style.border = "";
+    }
+  };
+
+  //item Tag의 input border 핸들러
+  const handleChangeItemTag = (e) => {
+    const value = e.target.value;
+    setItemTag(value);
+    if (value.trim().length > 5) {
+      e.target.style.border = "1px solid #f74747";
+    } else {
+      e.target.style.border = "";
+    }
   };
 
   return (
@@ -29,11 +78,13 @@ const RegistrationPage = () => {
         </label>
         <input
           className={style.inputItemTitle}
-          placeholder="상품을 입력해주세요"
+          placeholder="상품명을 입력해주세요"
           value={itemTitle}
-          onChange={handleiTitleChange}
+          onChange={handleChangeItemTitle}
         />
-        <div id="itemTitle-error-message"></div>
+        {itemTitle.trim().length > 10 && (
+          <div className={style.errorMessage}>10자 이내로 입력해주세요</div>
+        )}
       </form>
 
       <form>
@@ -43,9 +94,11 @@ const RegistrationPage = () => {
         <input
           className={style.inputItemContent}
           placeholder="상품 소개를 입력해주세요"
+          value={itemContent}
+          onChange={handleChangeItemContent}
         />
-        {itemTitle.trim().length >= 11 && (
-          <div id="itemContent-error-message">10자 이내로 입력해주세요</div>
+        {isContentError && (
+          <div className={style.errorMessage}>10자 이상 입력해주세요</div>
         )}
       </form>
 
@@ -56,8 +109,12 @@ const RegistrationPage = () => {
         <input
           className={style.inputItemPrice}
           placeholder="판매 가격을 입력해주세요"
+          value={itemPrice}
+          onChange={handleChangeItemPrice}
         />
-        <div id="itemPricd-error-message"></div>
+        {isNaN(itemPrice) && (
+          <div className={style.errorMessage}>숫자로 입력해주세요</div>
+        )}
       </form>
 
       <form>
@@ -66,9 +123,13 @@ const RegistrationPage = () => {
         </label>
         <input
           className={style.inputItemTag}
-          placeholder="태그를를 입력해주세요"
+          placeholder="태그를 입력해주세요"
+          value={itemTag}
+          onChange={handleChangeItemTag}
         />
-        <div id="itemTitle-error-message"></div>
+        {itemTag.trim().length > 5 && (
+          <div className={style.errorMessage}>5글자 이내로 입력해주세요</div>
+        )}
       </form>
     </div>
   );
