9deb19c099d338f6f8a5a71afb257593f87f737c
Decal <vumublne@naver.com>
2025-04-14 22:20:19 +0900

    chore: next.js 세팅

---
+++ src/app/layout.js
@@ -0,0 +1,31 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/common/Header/Header";
+import Footer from "@/components/common/Footer/Footer";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "판다마켓",
+  description: "일상에서 모든 물건을 거래해보세요",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
