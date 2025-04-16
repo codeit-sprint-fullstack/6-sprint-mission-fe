df6e1cc46badfc57fbb347dc8fd77fafe77f563d
fiivxyxxng <scx200800@gmail.com>
2025-04-16 18:53:57 +0900

    feat: 검색 및 정렬 기능 구현

--- src/app/(market)/board/_components/Article.jsx
+++ src/app/(market)/board/_components/Article.jsx
@@ -1,10 +1,38 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useMemo, useState } from "react";
 import ArticleCard from "./ArticleCard";
 import dayjs from "dayjs";
+import Dropdown from "@/components/ui/Dropdown";
 
 function Article({ articles }) {
+  const [searchInput, setSearchInput] = useState("");
+  const [sortOption, setSortOption] = useState("latest");
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const filteredArticles = useMemo(() => {
+    const filtered = articles.filter((article) =>
+      article.title.toLowerCase().includes(searchInput.toLowerCase())
+    );
+
+    if (sortOption == "latest") {
+      return filtered.sort(
+        (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+      );
+    } else if (sortOption == "oldest") {
+      return filtered.sort(
+        (a, b) => new Date(a.createdAt) - new Date(b.createdAt)
+      );
+    }
+  }, [articles, searchInput, sortOption]);
+
+  const handleSort = (type) => {
+    setSortOption(type);
+    setIsDropdownOpen(false);
+  };
+
   return (
     <div>
       <div className="flex justify-between items-center">
@@ -17,18 +45,25 @@ function Article({ articles }) {
         <input
           className="w-full mr-[13px] py-[9px] pl-11 rounded-xl bg-gray-100 bg-[url('/assets/icon/ic_search.svg')] bg-no-repeat bg-[center_left_1rem]"
           placeholder="검색할 상품을 입력해주세요"
+          onChange={(e) => setSearchInput(e.target.value)}
         />
-        <button className="p-[9px] border-1 border-gray-100 rounded-lg cursor-pointer">
-          <Image
-            src="/assets/icon/ic_sort.svg"
-            alt="정렬 아이콘"
-            width={24}
-            height={24}
-          />
-        </button>
+        <div>
+          <button
+            className="p-[9px] border-1 border-gray-100 rounded-lg cursor-pointer"
+            onClick={() => setIsDropdownOpen((prev) => !prev)}
+          >
+            <Image
+              src="/assets/icon/ic_sort.svg"
+              alt="정렬 아이콘"
+              width={24}
+              height={24}
+            />
+          </button>
+          {isDropdownOpen && <Dropdown handleSort={handleSort} />}
+        </div>
       </div>
       <article className="mb-[91px]">
-        {articles.map((article) => {
+        {filteredArticles.map((article) => {
           const date = article.createdAt;
           const formattedDate = dayjs(date).format("YYYY. MM. DD");
 
---
+++ src/components/ui/Dropdown.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Dropdown({ handleSort }) {
+  return (
+    <ul className="flex flex-col justify-center absolute mt-2 right-4 w-[139px] border-1 border-gray-200 rounded-lg bg-white cursor-pointer">
+      <li
+        className="text-center w-full pt-4 pb-3 hover:bg-gray-50"
+        onClick={() => handleSort("latest")}
+      >
+        최신순
+      </li>
+      <li
+        className="text-center w-full pt-4 pb-3 hover:bg-gray-50"
+        onClick={() => handleSort("oldest")}
+      >
+        오래된순
+      </li>
+    </ul>
+  );
+}
+
+export default Dropdown;
