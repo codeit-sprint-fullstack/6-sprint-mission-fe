b3b4c89121f77ead3fdee3cda30ea07dfd818acf
Jam1eL1 <23jisu@gmail.com>
2025-04-23 16:07:59 +0900

    style: update Button component focus setting to active

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,7 +1,7 @@
 export default function Button({ children, className = "", ...props }) {
   return (
     <button
-      className={`inline-flex items-center justify-center rounded-lg py-[11.5px] px-[23px] bg-primary hover:bg-primary-200 focus:bg-primary-300 text-white font-semibold w-[88px] h-[42px] cursor-pointer ${className}`}
+      className={`inline-flex items-center justify-center rounded-lg py-[11.5px] px-[23px] bg-primary hover:bg-primary-200 active:bg-primary-300 text-white font-semibold w-[88px] h-[42px] cursor-pointer ${className}`}
       {...props}
     >
       {children}
