fc93f5e9e001ea3cada93036a953fa9468614f46
Jam1eL1 <23jisu@gmail.com>
2025-04-25 17:51:58 +0900

    feat: implement user login functionality

--- src/app/login/_components/LoginForm.jsx
+++ src/app/login/_components/LoginForm.jsx
@@ -5,6 +5,7 @@ import AuthSubmitButton from "@/components/ui/AuthSubmitButton";
 import FormInput from "@/components/ui/FormInput";
 import PasswordInput from "@/components/ui/PasswordInput";
 import useLoginForm from "@/hooks/useLoginForm";
+import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 
 export default function LoginForm() {
@@ -18,16 +19,66 @@ export default function LoginForm() {
     handlePasswordChange,
   } = useLoginForm();
 
+  const router = useRouter();
+
+  // need to improve modal component logic by using Provider later
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [modalMessage, setModalMessage] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    if (isFormValid) {
-      console.log("Login form submitted");
+    // console.log("handleSubmit called");
+    if (!isFormValid) return;
+
+    try {
+      // console.log("api client called");
+      setIsLoading(true);
+      // send POST request to server
+      const response = await fetch(
+        "https://panda-market-api.vercel.app/auth/signIn",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email,
+            password,
+          }),
+        }
+      );
+
+      if (!response.ok) {
+        console.log("login error", errorData.message);
+        setModalMessage("비밀번호가 일치하지 않습니다.");
+        setIsModalOpen(true);
+        throw new Error("Login failed");
+      }
+      // use await again to get to the data
+      const data = await response.json();
+      // get the jwt token info
+      const accessToken = data.accessToken;
+      const refreshToken = data.refreshToken;
+
+      if (accessToken) {
+        // save access token to the user's localStorage
+        // console.log("token received", accessToken);
+        localStorage.setItem("accessToken", accessToken);
+        // console.log("refresh token received:", refreshToken);
+        localStorage.setItem("refreshToken", refreshToken);
+        // redirect user to the market page
+        router.push("/market");
+      } else {
+        // remove this line later
+        console.log("token not received");
+      }
+    } catch (error) {
+      setErrorMsg(error.message);
+    } finally {
+      setIsLoading(false);
     }
-    // if the form is valid then start sending request to API
-    // check password - email match and set the modalMessage
   };
   return (
     <>
---
+++ src/app/market/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function MarketPage() {
+  return (
+    <div>This is Market Page</div>
+  )
+}
