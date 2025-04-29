74a0b40e0c96a4e8e76b3697cd301a85cd5cd317
Jam1eL1 <23jisu@gmail.com>
2025-04-29 15:17:37 +0900

    feat: migrate itmes page

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,17 +1,16 @@
+const AUTH_API_BASE_URL =
+  process.env.NEXT_PUBLIC_AUTH_API_URL || "http://localhost:3001";
 export async function login({ email, password }) {
-  const response = await fetch(
-    "https://panda-market-api.vercel.app/auth/signIn",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email,
-        password,
-      }),
-    }
-  );
+  const response = await fetch(`${AUTH_API_BASE_URL}/auth/signIn`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email,
+      password,
+    }),
+  });
 
   if (!response.ok) {
     const errorData = await response.json();
@@ -21,21 +20,18 @@ export async function login({ email, password }) {
 }
 
 export async function signUp({ email, nickname, password, passwordConfirm }) {
-  const response = await fetch(
-    "https://panda-market-api.vercel.app/auth/signUp",
-    {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify({
-        email,
-        nickname,
-        password,
-        passwordConfirmation: passwordConfirm,
-      }),
-    }
-  );
+  const response = await fetch(`${AUTH_API_BASE_URL}/auth/signUp`, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      email,
+      nickname,
+      password,
+      passwordConfirmation: passwordConfirm,
+    }),
+  });
   if (!response.ok) {
     const errorData = await response.json();
     throw new Error(errorData.message);
--- src/app/items/_components/AllItemsSection.jsx
+++ src/app/items/_components/AllItemsSection.jsx
@@ -1,19 +1,71 @@
-import React from "react";
+"use client";
+import Button from "@/components/ui/Button";
+import DropdownMenu from "@/components/ui/DropdownMenu";
+import SearchInput from "@/components/ui/SearchInput";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import ItemCard from "./ItemCard";
+import PaginationBar from "@/components/ui/PaginationBar";
+import { getProducts } from "@/api/items";
 
 export default function AllItemsSection() {
+  const [orderBy, setOrderBy] = useState("recent");
+  const [page, setPage] = useState(1);
+  const [pageSize, setPageSize] = useState(20); // fixed size
+  const [totalPageNum, setTotalPageNum] = useState(0);
+  const [itemsList, setItemsList] = useState([]);
+  const [word, setWord] = useState("");
+
+  const fetchSortedData = async ({ orderBy, page, pageSize, word }) => {
+    const skip = (page - 1) * pageSize;
+    const take = pageSize;
+
+    const products = await getProducts({
+      orderBy,
+      skip,
+      take,
+      word: word ? word : undefined,
+    });
+
+    setItemsList(products.data);
+    setTotalPageNum(Math.ceil(products.count / pageSize));
+  };
+
+  useEffect(() => {
+    fetchSortedData({ orderBy, page, pageSize, word });
+  }, [orderBy, page, word]);
+
+  const onPageChange = (pageNumber) => {
+    setPage(pageNumber);
+  };
+
   return (
-    //wrapper
-    <div className="flex flex-col justify-center items-center gap-10 bg-amber-100 w-full">
-      {/* header */}
-      <div className="flex flex-col">
-        <h1 className="text-secondary-900 font-bold text-xl leading-8">판매 중인 상품</h1>
+    <div className="flex flex-col justify-center items-center gap-10 w-full h-full">
+      <div className="flex justify-between items-center w-full">
+        <h1 className="text-secondary-900 font-bold text-xl leading-8">
+          판매 중인 상품
+        </h1>
+        <Link href="items/create">
+          <Button className="w-[133px]">상품 등록하기</Button>
+        </Link>
       </div>
-      {/* items */}
-      <div>items list section</div>
-      {/* pagination bar */}
-      <div>
-        <p>pagination bar</p>
+
+      <div className="flex items-center justify-between">
+        <SearchInput value={word} onChange={(e) => setWord(e.target.value)} />
+        <DropdownMenu orderBy={orderBy} onSortChange={setOrderBy} />
       </div>
+
+      <div className="grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-6 w-full">
+        {itemsList.map((item) => (
+          <ItemCard key={item.id} item={item} />
+        ))}
+      </div>
+
+      <PaginationBar
+        totalPageNum={totalPageNum}
+        activePageNum={page}
+        onPageChange={onPageChange}
+      />
     </div>
   );
 }
