a09844acb0cacf16d14a8ada82944aec73b809cf
rlawodnr1109 <wodnr1109@naver.com>
2025-04-30 10:06:12 +0900

    feat: Add RouteGuard

--- FE/src/app/providers.jsx
+++ FE/src/app/providers.jsx
@@ -1,12 +1,13 @@
-"use client";
-
 import AuthProvider from "@/providers/AuthProvider";
+import RouteGuard from "@/providers/RouteGuard";
 import ThemeProvider from "@/providers/ThemeProvider";
 
 export default function Providers({ children }) {
   return (
-    <ThemeProvider>
-      <AuthProvider>{children}</AuthProvider>
-    </ThemeProvider>
+    <AuthProvider>
+      <ThemeProvider>
+        <RouteGuard>{children}</RouteGuard>
+      </ThemeProvider>
+    </AuthProvider>
   );
 }
