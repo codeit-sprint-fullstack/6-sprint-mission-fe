db71627e7b853e340b71f2db10b50b3bc61c988c
Decal <vumublne@naver.com>
2025-06-20 11:09:18 +0900

    fix: Image 컴포넌트 오류 수정

--- src/app/products/_components/BestProducts/BestProduct.tsx
+++ src/app/products/_components/BestProducts/BestProduct.tsx
@@ -21,15 +21,18 @@ export default function BestProduct({ bestProduct }: IBestProductProps) {
       href={`/products/${bestProduct.id}`}
       className="flex flex-col justify-center items-start gap-[10px]"
     >
-      <Image
-        src={
-          bestProduct?.images?.length === 0
-            ? img_default_product.src
-            : bestProduct?.images?.[0]
-        }
-        alt={bestProduct?.name}
-        className="relative w-full h-full aspect-[1/1] rounded-[12px] overflow-hidden"
-      />
+      <div className="relative w-full h-full aspect-[1/1] rounded-[12px] overflow-hidden">
+        <Image
+          src={
+            bestProduct?.images?.length === 0
+              ? img_default_product
+              : bestProduct?.images?.[0]
+          }
+          alt={bestProduct?.name}
+          fill
+          className="object-cover"
+        />
+      </div>
       <div className="flex flex-col justify-center items-start gap-[6px]">
         <p className="font-medium text-[14px]/[24px]">{bestProduct?.name}</p>
         <p className="font-bold text-[16px]/[26px]">
